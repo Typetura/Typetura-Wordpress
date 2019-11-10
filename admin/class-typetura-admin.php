eea2f1bc06df199559a8eb5107e9ae44dbd7b1b3
Salvador Hernandez <s.hernandez5400@gmail.com>
2019-11-10 15:54:53 -0500

    Initialize and Add a Setting Page

--- admin/class-typetura-admin.php
+++ admin/class-typetura-admin.php
@@ -100,4 +100,51 @@ class Typetura_Admin {
 
 	}
 
+		/**
+	 * Register the administration menu for this plugin into the WordPress Dashboard menu.
+	 *
+	 * @since    1.0.0
+	 */
+
+	public function add_plugin_admin_menu() {
+
+		/*
+		* Add a settings page for this plugin to the Settings menu.
+		*
+		* NOTE:  Alternative menu locations are available via WordPress administration menu functions.
+		*
+		*        Administration Menus: http://codex.wordpress.org/Administration_Menus
+		*
+		*/
+		add_options_page( 'Typetura Settings', 'Typetura Settings', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page')
+		);
+	}
+
+	/**
+	* Add settings action link to the plugins page.
+	*
+	* @since    1.0.0
+	*/
+
+	public function add_action_links( $links ) {
+		/*
+		*  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
+		*/
+	$settings_link = array(
+		'<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+	);
+	return array_merge(  $settings_link, $links );
+
+	}
+
+	/**
+	* Render the settings page for this plugin.
+	*
+	* @since    1.0.0
+	*/
+
+	public function display_plugin_setup_page() {
+		include_once( 'partials/typetura-public-display.php' );
+	}
+
 }
--- includes/class-typetura.php
+++ includes/class-typetura.php
@@ -157,6 +157,16 @@ class Typetura {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
+		// Save/Update our plugin options
+		$this->loader->add_action( 'admin_init', $plugin_admin, 'options_update');
+
+		// Add menu item
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_plugin_admin_menu' );
+
+		// Add Settings link to the plugin
+		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
+		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
+
 	}
 
 	/**
