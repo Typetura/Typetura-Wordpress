49edfb70b66229fdfddd76c6ba087ed5e4a90784
Salvador Hernandez <s.hernandez5400@gmail.com>
2019-12-08 16:55:11 -0500

    show package preview

--- admin/partials/typetura-admin-display.php
+++ admin/partials/typetura-admin-display.php
@@ -17,7 +17,7 @@
 
 <div class="wrap">
 
-	<h2 style="margin-bottom: 10px;" class="nav-tab-wrapper"><?php echo esc_html( get_admin_page_title() ); ?></h2>
+	<h2 style="padding-bottom: 10px; margin-bottom: 20px;" class="nav-tab-wrapper"><?php echo esc_html( get_admin_page_title() ); ?></h2>
 
 	<form method="post" name="typetura_settings" action="options.php">
 
@@ -37,9 +37,32 @@
 		do_settings_sections( $this->plugin_name );
 	?>
 
-	<fieldset>
+	<fieldset style="font-size: 16px;">
+		<div>
+			<?php _e('Enter your Typetura API key. You can Find it in your <a href="https://typetura.com/account-settings">Typetura account settings</a>', $this->plugin_name);?>
+		</div>
+		<div>
+			<?php _e('Don\'t have an account? <a href="https://typetura.com/auth/create-account">Create a Typetura account here.</a>', $this->plugin_name);?>
+		</div>
+		<legend class="screen-reader-text">
+			<span>
+				<?php _e('Type in your typetura API key', $this->plugin_name);?>
+			</span>
+		</legend>
+		<input
+			type="text"
+			class="regular-text"
+			id="<?php echo $this->plugin_name;?>-typetura_api_key"
+			name="<?php echo $this->plugin_name;?>[typetura_api_key]"
+			value="<?php if(!empty($typetura_api_key)) echo $typetura_api_key;?>"
+		/>
+	</fieldset>
+
+	<div style="padding: 10px 0;"></div>
+
+	<fieldset style="font-size: 16px;">
 		<legend class="screen-reader-text"><span><?php _e('Select Typetura Package', $this->plugin_name);?></span></legend>
-		<div><?php _e('Select Typetura Package', $this->plugin_name);?></div>
+		<div><?php _e('Select a Typetura Package. You can <a href="https://typetura.com/typography-packages">browse the packages here.</a>', $this->plugin_name);?></div>
 
 		<label for="<?php echo $this->plugin_name;?>-typetura_package">
 			<select
@@ -60,28 +83,21 @@
 				<option value="zine" <?php echo $selected; ?>>Zine</option>
 			</select>
 		</label>
-	</fieldset>
 
-	<fieldset>
 		<div>
-			<?php _e('Typetura API Key', $this->plugin_name);?>
+			<img id="package-preview" src="https://s3.amazonaws.com/typetura.com/production/<?php echo $typetura_package ?>/preview.png" width="600" height="300" alt=" " class="package_image">
 		</div>
-		<legend class="screen-reader-text">
-			<span>
-				<?php _e('Type in your typetura API key', $this->plugin_name);?>
-			</span>
-		</legend>
-		<input
-			type="text"
-			class="regular-text"
-			id="<?php echo $this->plugin_name;?>-typetura_api_key"
-			name="<?php echo $this->plugin_name;?>[typetura_api_key]"
-			value="<?php if(!empty($typetura_api_key)) echo $typetura_api_key;?>"
-		/>
 	</fieldset>
 
 	<?php submit_button(__('Save all changes', $this->plugin_name), 'primary','submit', TRUE); ?>
 
   </form>
 
+	<script>
+		document.getElementById('typetura-typetura_package').addEventListener('change', function(e) {
+			var packageName = e.target.value;
+
+			document.getElementById('package-preview').src = "https://s3.amazonaws.com/typetura.com/production/" + packageName + '/preview.png';
+		});
+	</script>
 </div>
