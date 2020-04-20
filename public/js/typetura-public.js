2f247ed60f616e89b772768717700224bbaf5901
Scott Kellum <scott@typetura.com>
2020-04-20 16:58:12 -0700

    Tweak script

--- public/js/typetura-public.js
+++ public/js/typetura-public.js
@@ -5,14 +5,17 @@ document.onreadystatechange = function() {
 		const secondaryHeadline = document.querySelectorAll("h2");
 		const secondarySubheadline = document.querySelectorAll("h3");
 
-		primaryHeadline.classList.add("primary-headline");
-		primarySubheadline.classList.add("primary-subheadline");
 		secondaryHeadline.forEach(function(e) {
 			e.classList.add("secondary-headline");
 		});
 		secondarySubheadline.forEach(function(e) {
 			e.classList.add("secondary-subheadline");
 		});
+		primarySubheadline.classList.remove("secondary-headline");
+		primarySubheadline.classList.remove("secondary-subheadline");
+		primarySubheadline.classList.add("primary-subheadline");
+		primaryHeadline.classList.remove("secondary-headline");
+		primaryHeadline.classList.add("primary-headline");
 
 		typeturaInit(document.querySelectorAll(typeturaContexts));
 	}
