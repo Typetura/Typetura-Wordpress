0d5e3246adc0050cc57e0b1a28b9820a785edae4
Scott Kellum <scott@typetura.com>
2020-05-23 15:06:59 -0700

    meta tag now queried in js

--- public/js/typetura-public.js
+++ public/js/typetura-public.js
@@ -1,18 +1,22 @@
-const primaryHeadline = document.querySelectorAll(["h1", ".site-title"]);
-const primarySubheadline = document.querySelectorAll(["h1 + h2", ".site-description"]);
-const secondaryHeadline = document.querySelectorAll("h2");
-const secondarySubheadline = document.querySelectorAll("h3");
+const typetura_meta = document.querySelectorAll(["[class*='meta']"]);
+const typetura_primaryHeadline = document.querySelectorAll(["h1", ".site-title"]);
+const typetura_primarySubheadline = document.querySelectorAll(["h1 + h2", ".site-description"]);
+const typetura_secondaryHeadline = document.querySelectorAll("h2");
+const typetura_secondarySubheadline = document.querySelectorAll("h3", "h4", "h5", "h6");
 
-secondaryHeadline.forEach(function(e) {
+typetura_meta.forEach(function(e) {
+	e.classList.add("meta");
+});
+typetura_secondaryHeadline.forEach(function(e) {
 	e.classList.add("section-headline");
 });
-secondarySubheadline.forEach(function(e) {
+typetura_secondarySubheadline.forEach(function(e) {
 	e.classList.add("section-subheadline");
 });
-primaryHeadline.forEach(function(e) {
+typetura_primaryHeadline.forEach(function(e) {
 	e.classList.add("primary-headline");
 });
-primarySubheadline.forEach(function(e) {
+typetura_primarySubheadline.forEach(function(e) {
 	e.classList.remove("secondary-headline");
 	e.classList.add("primary-subheadline");
 });
